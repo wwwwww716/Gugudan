947516522c3b1f211772ba24f4830d09da51bac5
m1 <wwwwww716@naver.com>
2023-04-03 22:58:22 +0900

    사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다. 팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -4,16 +4,17 @@ public class Gugudan {
     public static void main(String[] args) {
         System.out.println("숫자를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println("Number" + n + "단");
+        String inputValue = scanner.nextLine();
+        String[] splitedValue = inputValue.split(",");
+        int first = Integer.parseInt(splitedValue[0]);
+        int second = Integer.parseInt(splitedValue[1]);
 
-        int[] result = new int[n];
-
-        for (int i = 2; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                System.out.println(i + "*" + j + "=" + (i * j));
+        for (int i = 2; i<= first; i++) {
+            for (int j = 1; j <= second; j++) {
+                System.out.println(i + "*" + j + "=" + i * j);
             }
         }
-    }
 
+
+    }
 }
