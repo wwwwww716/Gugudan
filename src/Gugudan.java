d04f4ed14d8e79578e6937e6d797467abc2714db
m1 <wwwwww716@naver.com>
2023-04-03 21:29:49 +0900

    구구단에서 4단과 5단을 계산한 결과 값을 변수에 저장한 후 저장한 변수 값을 출력한다.

---
+++ src/Gugudan.java
@@ -0,0 +1,43 @@
+public class Gugudan {
+    public static void main(String[] args) {
+
+        int result = 4 * 1;
+        System.out.println(result);
+        result = 4 * 2;
+        System.out.println(result);
+        result = 4 * 3;
+        System.out.println(result);
+        result = 4 * 4;
+        System.out.println(result);
+        result = 4 * 5;
+        System.out.println(result);
+        result = 4 * 6;
+        System.out.println(result);
+        result = 4 * 7;
+        System.out.println(result);
+        result = 4 * 8;
+        System.out.println(result);
+        result = 4 * 9;
+        System.out.println(result);
+        result = 5 * 1;
+        System.out.println(result);
+        result = 5 * 2;
+        System.out.println(result);
+        result = 5 * 3;
+        System.out.println(result);
+        result = 5 * 4;
+        System.out.println(result);
+        result = 5 * 5;
+        System.out.println(result);
+        result = 5 * 6;
+        System.out.println(result);
+        result = 5 * 7;
+        System.out.println(result);
+        result = 5 * 8;
+        System.out.println(result);
+        result = 5 * 9;
+        System.out.println(result);
+
+        // 구구단에서 4단과 5단을 계산한 결과 값을 변수에 저장한 후 저장한 변수 값을 출력한다.
+    }
+}
