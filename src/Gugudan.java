3a583eb835c27a3bda1102cd3a8e24927a11c647
m1 <wwwwww716@naver.com>
2023-04-03 21:41:47 +0900

    숫자 입력하면 해당 숫자 단 전체를 계산해 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,43 +1,36 @@
+import java.util.Scanner;
+
 public class Gugudan {
     public static void main(String[] args) {
 
-        int result = 4 * 1;
+        System.out.println("구구단 중 출력할 단은? :");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("Number : " + n + "단");
+
+        int result = n * 1;
         System.out.println(result);
-        result = 4 * 2;
+        result = n * 2;
         System.out.println(result);
-        result = 4 * 3;
+        result = n * 3;
         System.out.println(result);
-        result = 4 * 4;
+        result = n * 4;
         System.out.println(result);
-        result = 4 * 5;
+        result = n * 5;
         System.out.println(result);
-        result = 4 * 6;
+        result = n * 6;
         System.out.println(result);
-        result = 4 * 7;
+        result = n * 7;
         System.out.println(result);
-        result = 4 * 8;
+        result = n * 8;
         System.out.println(result);
-        result = 4 * 9;
-        System.out.println(result);
-        result = 5 * 1;
-        System.out.println(result);
-        result = 5 * 2;
-        System.out.println(result);
-        result = 5 * 3;
-        System.out.println(result);
-        result = 5 * 4;
-        System.out.println(result);
-        result = 5 * 5;
-        System.out.println(result);
-        result = 5 * 6;
-        System.out.println(result);
-        result = 5 * 7;
-        System.out.println(result);
-        result = 5 * 8;
-        System.out.println(result);
-        result = 5 * 9;
+        result = n * 9;
         System.out.println(result);
 
-        // 구구단에서 4단과 5단을 계산한 결과 값을 변수에 저장한 후 저장한 변수 값을 출력한다.
+
+
+
     }
 }
