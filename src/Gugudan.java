4d711f5b0dc69525735b90b0568c19f7a384e29e
m1 <wwwwww716@naver.com>
2023-04-03 21:56:35 +0900

    2단을 크기가 9인 배열을 생성하여 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class Gugudan {
     public static void main(String[] args) {
 
-    System.out.println("구구단 중 출력할 단은?");
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    System.out.println("Number : " + n + "단");
-
-    if (2 <= n && n <= 9) {
-        for (int i = 1; i<10; i++) {
-            System.out.println(n * i);
+        int[] result = new int[9];
+        for (int i = 0; i<result.length; i++) {
+            result[i] = 2 * (i+1);
         }
-    }  else {
-        System.out.println("2이상 9이하의 값만 입력할 수 있습니다.");
-    }
 
+        for (int i = 0; i<result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
