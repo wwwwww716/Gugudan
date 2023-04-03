b41f3f7ce1918c5617de022460e55cad98bf5312
m1 <wwwwww716@naver.com>
2023-04-03 21:57:39 +0900

    for문을 하나로 합쳐서 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -6,10 +6,8 @@ public class Gugudan {
         int[] result = new int[9];
         for (int i = 0; i<result.length; i++) {
             result[i] = 2 * (i+1);
-        }
-
-        for (int i = 0; i<result.length; i++) {
             System.out.println(result[i]);
         }
+
     }
 }
