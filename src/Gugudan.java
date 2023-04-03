aa4cf2b8b7c30270088a057dc11d2cbee8398638
m1 <wwwwww716@naver.com>
2023-04-03 22:49:56 +0900

    사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
 public class Gugudan {
-    public static int[] calculate(int times) {
-        int[] result = new int[9];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = times * (i+1);
-        } return result;
-    }
-
-    public static void print(int[] result) {
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-    }
-
     public static void main(String[] args) {
-        for (int i = 2; i < 10; i++) {
-            int[] result = calculate(i);
-            print(result);
+        System.out.println("숫자를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Number" + n + "단");
+
+        int[] result = new int[n];
+
+        for (int i = 2; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.println(i + "*" + j + "=" + (i * j));
+            }
         }
     }
+
 }
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,8 +1,5 @@
 public class GugudanMain {
     public static void main(String[] args) {
-        for (int i = 2; i < 10; i++) {
-            int[] result = Gugudan.calculate(i);
-            Gugudan.print(result);
-        }
+
     }
 }
